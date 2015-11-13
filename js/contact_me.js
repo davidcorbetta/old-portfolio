bbd4708cabc4d14d716cab0b6511aa9182e8c8d4
David Corbetta <david.corbetta@db1.com.br>
2015-11-13 16:53:43 -0200

    Atualizando site com profile, etc

--- js/contact_me.js
+++ js/contact_me.js
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Sua mensagem foi enviada.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Desculpe "+ firstName +", parece que o meu servidor de correio não está respondendo. Por favor tente de novo mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
